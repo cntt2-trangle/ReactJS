6701c734e2bd68351a5ebfec50e54eea24522fdc
Admin <le7207902@gmail.com>
2019-06-21 12:27:05 +0700

    [PHAN-02] Dinh nghia component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +9,34 @@ function One() {
       <h3>Kieu ham binh thuong</h3>
     </div>
   );
+}
+
+var Two=function(){
+  return (
+    <div>
+      <h1>Cach so 2</h1>
+      <h3>Kieu ham anonymous</h3>
+    </div>
+  );
+}
+
+var Three = () => (
+                    <div>
+                      <h1>Cach so 3</h1>
+                      <h3>Kieu ham arrow</h3>
+                    </div>
+                  );
+
+
+class Four extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Cach so 4</h1>
+        <h3>Kieu class</h3>
+      </div>
+    );
+  }
 }
 
 function App() {
@@ -29,6 +57,12 @@ function App() {
         </a>
       </header>
       <One></One>
+      <hr></hr>
+      <Two></Two>
+      <hr></hr>
+      <Three></Three>
+      <hr></hr>
+      <Four></Four>
     </div>
   );
 }
